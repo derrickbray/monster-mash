a21cb0db949d1ffcadc409c0fd7ea743d6ef74c6
Derrick Bray <derrickbray@yahoo.com>
2016-10-31 17:44:33 -0500

    left style layout

--- app/app.vue
+++ app/app.vue
@@ -1,37 +1,35 @@
 <template lang="html">
   <div class="app">
-    <div class="flex-box">
-      <div class="flex-box__left">
+    <div class="app__left">
 
-        <h1>Build Your Monster</h1>
+      <h1>Build Your Monster</h1>
 
-        <div class="img-switcher">
-          <button class="btn"><i class="fa fa-caret-left fa-2x" aria-hidden="true"></i></button>
-          <div class="frame">
-            <img src="/monsters/body-1.png" alt="part.title"/>
-          </div>
-          <button class="btn"><i class="fa fa-caret-right fa-2x" aria-hidden="true"></button>
+      <div class="img-switcher">
+        <button class="btn"><i class="fa fa-caret-left fa-2x" aria-hidden="true"></i></button>
+        <div class="frame">
+          <img src="/monsters/body-1.png" alt="part.title"/>
         </div>
-
-        <div class="img-switcher">
-          <button class="btn"><i class="fa fa-caret-left fa-2x" aria-hidden="true"></i></button>
-          <div class="frame">
-            <img src="/monsters/mouth-1.png" alt="part.title"/>
-          </div>
-          <button class="btn"><i class="fa fa-caret-right fa-2x" aria-hidden="true"></button>
-        </div>
-
-        <div class="img-switcher">
-          <button class="btn"><i class="fa fa-caret-left fa-2x" aria-hidden="true"></i></button>
-          <div class="frame">
-            <img src="/monsters/eyes-1.png" alt="part.title"/>
-          </div>
-          <button class="btn"><i class="fa fa-caret-right fa-2x" aria-hidden="true"></button>
-        </div>
-
+        <button class="btn"><i class="fa fa-caret-right fa-2x" aria-hidden="true"></button>
       </div>
-      <div class="flex-box__right"></div>
+
+      <div class="img-switcher">
+        <button class="btn"><i class="fa fa-caret-left fa-2x" aria-hidden="true"></i></button>
+        <div class="frame">
+          <img src="/monsters/mouth-1.png" alt="part.title"/>
+        </div>
+        <button class="btn"><i class="fa fa-caret-right fa-2x" aria-hidden="true"></button>
+      </div>
+
+      <div class="img-switcher">
+        <button class="btn"><i class="fa fa-caret-left fa-2x" aria-hidden="true"></i></button>
+        <div class="frame">
+          <img src="/monsters/eyes-1.png" alt="part.title"/>
+        </div>
+        <button class="btn"><i class="fa fa-caret-right fa-2x" aria-hidden="true"></button>
+      </div>
+
     </div>
+    <div class="app__right"></div>
   </div>
 </template>
 
@@ -45,8 +43,7 @@ export default Vue.extend({
     }
   },
 
-  mounted: {
-  },
+  mounted: {},
 
 
   methods: {
